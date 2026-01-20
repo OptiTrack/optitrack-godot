e08c35417226271d30d076a3b2d0d6398f975cc0
michaelhunter27 <michael.max.hunter27@gmail.com>
2026-01-20 15:01:23 -0800

    Enable MotiveRigidBody to animate in the editor
    Adds the boolean property animate_in_editor to the MotiveRigidBody
    class. The rigid body will animate in the editor when connected to
    Motive.

--- example-project/addons/optitrack_plugin/motive_rigid_body/motiverigidbody.gd
+++ example-project/addons/optitrack_plugin/motive_rigid_body/motiverigidbody.gd
@@ -1,17 +1,16 @@
+@tool
 extends Node3D
 
-var rigid_body_asset_ID : int
+@export var rigid_body_asset_ID : int = 0
+@export var animate_in_editor : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	rigid_body_asset_ID = 0
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position = Motive.get_rigid_body_pos(rigid_body_asset_ID)
-	quaternion = Motive.get_rigid_body_rot(rigid_body_asset_ID)
-
-
-func _enter_tree() -> void:
-	pass
+	if not Engine.is_editor_hint() or animate_in_editor:
+		position = MotiveAutoload.get_rigid_body_pos(rigid_body_asset_ID)
+		quaternion = MotiveAutoload.get_rigid_body_rot(rigid_body_asset_ID)
--- example-project/addons/optitrack_plugin/motive_rigid_body/motiverigidbodyplugin.gd
+++ example-project/addons/optitrack_plugin/motive_rigid_body/motiverigidbodyplugin.gd
@@ -2,16 +2,6 @@
 extends EditorPlugin
 
 
-func _enable_plugin() -> void:
-	# Add autoloads here.
-	pass
-
-
-func _disable_plugin() -> void:
-	# Remove autoloads here.
-	pass
-
-
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
 	# Add the new type with a name, a parent type, a script and an icon.
@@ -21,3 +11,13 @@ func _enter_tree() -> void:
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
 	remove_custom_type("MotiveRigidBody")
+
+
+func _enable_plugin() -> void:
+	# Add autoloads here.
+	pass
+
+
+func _disable_plugin() -> void:
+	# Remove autoloads here.
+	pass
